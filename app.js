277ae562109e8c7fff216785cb6476470bff6db4
Rex Munil <lubwamaemmanuel1@gmail.com>
2022-08-03 11:48:42 +0300

    Prepared for production

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const cors = require('cors')
 const logger = require("morgan");
 const pug = require("pug");
 
@@ -16,6 +17,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
